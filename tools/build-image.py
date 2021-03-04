bc7ada07fd6df78882eafb9207cdace83008bfa7
Samuel Jimenez <sjimenezre@gmail.com>
2021-03-04 20:40:58 +0100

    Instance creation via API

---
+++ tools/build-image.py
@@ -0,0 +1,57 @@
+import googleapiclient.discovery
+
+import config as conf
+
+compute = googleapiclient.discovery.build('compute', 'v1')
+
+### Create EC2 instance to setup MeiliSearch
+
+print('Creating GCP Compute instance')
+
+source_image = compute.images().getFromFamily(
+    project='debian-cloud',
+    family=conf.DEBIAN_BASE_IMAGE_FAMILY
+).execute()['selfLink']
+print(source_image)
+
+# DOCS: https://cloud.google.com/compute/docs/reference/rest/v1/instances/insert
+config = {
+        'name': conf.INSTANCE_BUILD_NAME,
+        'machineType': conf.INSTANCE_TYPE,
+        'disks': [
+            {
+                'boot': True,
+                'autoDelete': True,
+                'initializeParams': {
+                    'sourceImage': source_image,
+                }
+            }
+        ],
+
+        # Specify a network interface with NAT to access the public
+        # internet.
+        'networkInterfaces': [{
+            'network': 'global/networks/marketplace-vpc',
+            'accessConfigs': [
+                {'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT'}
+            ]
+        }],
+
+        #user-data
+        'metadata': {
+            'items': [{
+                # Startup script is automatically executed by the
+                # instance upon startup.
+                'key': 'user-data',
+                'value': conf.USER_DATA,
+            }]
+        }
+
+    }
+
+resp = compute.instances().insert(
+        project=conf.GCP_DEFAULT_PROJECT,
+        zone=conf.GCP_DEFAULT_ZONE,
+        body=config).execute()
+
+print(resp)
---
+++ tools/config.py
@@ -0,0 +1,31 @@
+import requests
+from datetime import datetime
+
+# Update with the MeiliSearch version TAG you want to build the AMI with
+
+MEILI_CLOUD_SCRIPTS_VERSION_TAG='v0.19.0'
+
+### Setup environment and settings
+
+DEBIAN_BASE_IMAGE_FAMILY='debian-10'
+
+USER_DATA =requests.get(
+    'https://raw.githubusercontent.com/meilisearch/cloud-scripts/{}/scripts/cloud-config.yaml'
+    .format(MEILI_CLOUD_SCRIPTS_VERSION_TAG)
+).text
+
+SNAPSHOT_NAME="meilisearch-{}-{}".format(
+    MEILI_CLOUD_SCRIPTS_VERSION_TAG,
+    DEBIAN_BASE_IMAGE_FAMILY
+    ).replace('.', '-')
+
+INSTANCE_BUILD_NAME="{}-build-{}".format(SNAPSHOT_NAME, datetime.now().strftime("-%d-%m-%Y-%H-%M-%S"))
+
+
+GCP_DEFAULT_PROJECT='meilisearchimage'
+GCP_DEFAULT_ZONE='us-central1-a'
+
+
+INSTANCE_TYPE='zones/{}/machineTypes/n1-standard-1'.format(GCP_DEFAULT_ZONE)
+
+MEILISEARCH_LOGO_URL='https://github.com/meilisearch/integration-guides/blob/main/assets/logos/logo.svg'
