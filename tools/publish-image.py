dd10e11b0217dcd082faf24a6c939a70dafa2acb
Samuel Jimenez <sjimenezre@gmail.com>
2021-03-10 17:49:55 +0100

    Documentation links

---
+++ tools/publish-image.py
@@ -0,0 +1,6 @@
+# Build image using projects.builds()
+#
+# https://cloud.google.com/compute/docs/images/export-image#exporting_an_image_with_a_single_command
+# https://cloud.google.com/build/docs/api/reference/rest/v1/projects.builds
+
+# Make image public
