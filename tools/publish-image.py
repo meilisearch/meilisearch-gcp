a37e71cf4f19c5fcd1ca135a1cc8c05107a1c29a
Samuel Jimenez <sjimenezre@gmail.com>
2021-03-15 18:52:55 +0100

    Make exported image publicly accessible

--- tools/publish-image.py
+++ tools/publish-image.py
@@ -3,9 +3,10 @@
 # https://cloud.google.com/compute/docs/images/export-image#exporting_an_image_with_a_single_command
 # https://cloud.google.com/build/docs/api/reference/rest/v1/projects.builds
 
-# Make image public
+# Export image to bucket
 
 import googleapiclient.discovery
+from google.cloud import storage
 
 import config as conf
 import utils
@@ -34,13 +35,23 @@ export_image = cloudbuild.projects().builds().create(
     }
 ).execute()
 
-print("Waiting for image publish operation")
-image_publication = utils.wait_for_build_operation(
+print("Waiting for image export operation")
+image_export = utils.wait_for_build_operation(
     cloudbuild=cloudbuild, 
     project=conf.GCP_DEFAULT_PROJECT, 
     operation=export_image['metadata']['build']['id']
 )
-if image_publication == utils.STATUS_OK:
-    print('   Image published')
+if image_export == utils.STATUS_OK:
+    print('   Image exported: {}'.format(conf.IMAGE_DESTINATION_URI))
 else:
-    print('   Timeout waiting for image publication')
+    print('   Timeout waiting for image export')
+
+# Make image public
+
+print("Publishing image")
+bucket = storage.Client().get_bucket(conf.IMAGE_DESTINATION_BUCKET_NAME)
+policy = bucket.get_iam_policy(requested_policy_version=3)
+policy.bindings.append({"role": 'roles/storage.objectViewer', "members": {'allUsers'}})
+bucket.set_iam_policy(policy)
+print("   Image is now public")
+
