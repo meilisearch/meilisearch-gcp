fd2a818749a6769628267f4d11bcdb15c17fa169
nicolasvienot <nvienot@gmail.com>
2021-04-01 17:47:08 +0200

    Add destroy_image

---
+++ tools/destroy_image.py
@@ -0,0 +1,21 @@
+import sys
+import googleapiclient.discovery
+import config as conf
+
+compute = googleapiclient.discovery.build('compute', 'v1')
+
+if len(sys.argv) > 1:
+    SNAPSHOT_NAME = sys.argv[1]
+else:
+    raise Exception("No snapshot name specified")
+
+print("Destroying image named: {name}...".format(
+    name=SNAPSHOT_NAME))
+
+# Destroy image
+
+print('Deleting image...')
+delete = compute.images().delete(project=conf.GCP_DEFAULT_PROJECT,
+                                 image=SNAPSHOT_NAME).execute()
+print(delete)
+print('Image deleted')
